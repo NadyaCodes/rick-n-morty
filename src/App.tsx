ecfb42da129783b455a96a284303e850645b1de5
NadyaCodes <nadyacorscadden@gmail.com>
2022-11-12 12:14:12 -0600

    create store and initial reducer and interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,48 @@
+// import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+
+// function App() {
+//   const sum = (a: number, b: number): number => {
+//     return a + b;
+//   };
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//           {sum(14, 33)}
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// @ts-ignore
+import { Store } from "./Store.tsx";
 
-function App() {
-  const sum = (a: number, b: number): number => {
-    return a + b;
-  };
+// https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes
 
+export default function App(): JSX.Element {
+  const store = React.useContext(Store);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          {sum(14, 33)}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      {console.log(store)}
+      <h1>Rick and Morty</h1>
+      <p>Pick your favourite episode!!!</p>
+    </React.Fragment>
   );
 }
-
-export default App;
